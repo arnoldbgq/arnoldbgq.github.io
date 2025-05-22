3f8d5e055adb3dff83667709b80943e85019ec37
arnoldbgq <arnoldb.gq@gmail.com>
2025-05-22 18:53:36 -0500

    d

--- entregable/paginabienvenida/js/scripts.js
+++ entregable/paginabienvenida/js/scripts.js
@@ -1,6 +1,7 @@
 // Esperar a que el DOM esté completamente cargado
 document.addEventListener("DOMContentLoaded", function () {
   const form = document.getElementById("contactoForm");
+  let formLR = this.document.getElementById("ReclamacionesForm");
 
   form.addEventListener("submit", function (e) {
     e.preventDefault(); // Evita que el formulario se envíe realmente
@@ -9,5 +10,13 @@ document.addEventListener("DOMContentLoaded", function () {
     alert(`Gracias ${nombre}, se ha registrado su mensaje.`);
 
     form.reset(); // Limpia el formulario
+  });
+
+  formLR.addEventListener("submit", function(e){
+    e.preventDefault();
+
+    const emailLR = document.getElementById("email").value;
+    alert(`Su reclamo fue registrado. Recibira un mensaje de confirmación en su Email ${emailLR}.´);
+        form.reset();
   });
 });
