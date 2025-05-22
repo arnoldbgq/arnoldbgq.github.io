a2bf33dc8aa4f2e32f10f60c15e2c77107ce6954
arnoldbgq <arnoldb.gq@gmail.com>
2025-05-22 18:36:25 -0500

    s

--- entregable/paginabienvenida/js/scripts.js
+++ entregable/paginabienvenida/js/scripts.js
@@ -1,7 +1,13 @@
-/*!
-* Start Bootstrap - Shop Homepage v5.0.6 (https://startbootstrap.com/template/shop-homepage)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-homepage/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+// Esperar a que el DOM esté completamente cargado
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("contactoForm");
+
+  form.addEventListener("submit", function (e) {
+    e.preventDefault(); // Evita que el formulario se envíe realmente
+
+    const nombre = document.getElementById("nombre").value;
+    alert(`Gracias ${nombre}, se ha registrado su mensaje.`);
+
+    form.reset(); // Limpia el formulario
+  });
+});
