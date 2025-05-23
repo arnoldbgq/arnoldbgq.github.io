aea3f82055a6f842cbc4e4c9021e333e0631ad74
arnoldbgq <arnoldb.gq@gmail.com>
2025-05-22 19:07:52 -0500

    js

--- entregable/paginabienvenida/js/scripts.js
+++ entregable/paginabienvenida/js/scripts.js
@@ -1,22 +1,24 @@
 // Esperar a que el DOM esté completamente cargado
 document.addEventListener("DOMContentLoaded", function () {
+  // Formulario de contacto
   const form = document.getElementById("contactoForm");
-  let formLR = document.getElementById("ReclamacionesForm");
+  if (form) {
+    form.addEventListener("submit", function (e) {
+      e.preventDefault();
+      const nombre = document.getElementById("nombre").value;
+      alert(`Gracias ${nombre}, se ha registrado su mensaje.`);
+      form.reset();
+    });
+  }
 
-  form.addEventListener("submit", function (e) {
-    e.preventDefault(); // Evita que el formulario se envíe realmente
-
-    const nombre = document.getElementById("nombre").value;
-    alert(`Gracias ${nombre}, se ha registrado su mensaje.`);
-
-    form.reset(); // Limpia el formulario
-  });
-
-  formLR.addEventListener("submit", function(e){
-    e.preventDefault();
-
-    const emailLR = document.getElementById("email").value;
-    alert(`Su reclamo fue registrado. Recibira un mensaje de confirmación en su Email ${emailLR}.`);
-        formLR.reset();
-  });
+  // Formulario de reclamos
+  const formLR = document.getElementById("ReclamacionesForm");
+  if (formLR) {
+    formLR.addEventListener("submit", function (e) {
+      e.preventDefault();
+      const emailLR = document.getElementById("email").value;
+      alert(`Su reclamo fue registrado. Recibirá un mensaje de confirmación en su Email ${emailLR}.`);
+      formLR.reset();
+    });
+  }
 });
